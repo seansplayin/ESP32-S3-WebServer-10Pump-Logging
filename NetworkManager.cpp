f88962db8b9f077133991e3d6f004da5d38446cd
seansplayin <seansplayin@hotmail.com>
2024-04-07 10:40:47 -0600

    Update NetworkManager.cpp

--- NetworkManager.cpp
+++ NetworkManager.cpp
@@ -17,15 +17,13 @@ extern const int W5500_INT;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(10, 20, 90, 33); 
 
+
 // Initialize Ethernet connection
  void setupNetwork() {
     SPI.begin();
     ETH.begin(W5500_MISO, W5500_MOSI, W5500_SCK, W5500_SS, W5500_INT, 25, SPI3_HOST, mac);
-      if (ETH.linkUp()) {
-        Serial.println ();
-     Serial.print("Ethernet link is up");
-  } else {
-    Serial.println ();
-     Serial.print("Ethernet link is down");
-  }
+    delay(2000);
+    Serial.print(F("IP Address : "));
+    Serial.println(ETH.localIP());
 }
+
